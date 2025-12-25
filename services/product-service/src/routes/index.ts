0d72b0bedce71b1a3bdfcfdc54aba49a19771168
AizeN1337 <maciejkalata79@gmail.com>
2025-12-25 15:50:06 +0100

    Dockerfile added

--- services/product-service/src/routes/index.ts
+++ services/product-service/src/routes/index.ts
@@ -1,29 +1,13 @@
 import { Router } from 'express';
 
-import PATHS from '@src/common/constants/PATHS';
-import UserRoutes from './UserRoutes';
-
+import router from './ProductRouter';
 /******************************************************************************
                                 Setup
 ******************************************************************************/
 
 const apiRouter = Router();
 
-
-// ** Add UserRouter ** //
-
-// Init router
-const userRouter = Router();
-
-// Get all users
-userRouter.get(PATHS.Users.Get, UserRoutes.getAll);
-userRouter.post(PATHS.Users.Add, UserRoutes.add);
-userRouter.put(PATHS.Users.Update, UserRoutes.update);
-userRouter.delete(PATHS.Users.Delete, UserRoutes.delete);
-
-// Add UserRouter
-apiRouter.use(PATHS.Users._, userRouter);
-
+apiRouter.use(router);
 /******************************************************************************
                                 Export default
 ******************************************************************************/
